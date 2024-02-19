cf28f522e6f58bd46ea6da2415eee9b5fb98544f
AdithKrishna S <81422425+adithkrishnaa@users.noreply.github.com>
2024-02-19 09:53:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// var num =8;
+//  function display() {
+//     var num = 10;
+//     console.log(num);
+
+//  }
+//  display ();
+//  console.log(num);
+
+
+ let a= [1,2,3];
+ let b = a;
+ b.push(4);
+ console.log(a);
